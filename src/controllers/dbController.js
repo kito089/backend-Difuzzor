bc800db7c704ac560056d3df8ee05a9c45eb35e9
kito089 <jezarinan@gmail.com>
2025-12-05 09:12:50 -0600

    se arreglo el usuario no encontrado

--- src/controllers/dbController.js
+++ src/controllers/dbController.js
@@ -25,7 +25,8 @@ export async function obtenerAtributos(tabla) {
         `SELECT COLUMN_NAME 
          FROM INFORMATION_SCHEMA.COLUMNS 
          WHERE TABLE_SCHEMA = DATABASE()
-           AND TABLE_NAME = ?`,
+           AND TABLE_NAME = ?
+         ORDER BY ORDINAL_POSITION`,
         [tabla]
     );
 
